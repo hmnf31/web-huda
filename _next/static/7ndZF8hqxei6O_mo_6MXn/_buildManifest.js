f8cfc46461875f795200557256efc776966af5ca
hmnf31 <hmnf31@users.noreply.github.com>
2026-05-14 17:54:16 +0000

    deploy: b4a7929e1de8a64892ef5063662606ea292b60c1

---
+++ _next/static/7ndZF8hqxei6O_mo_6MXn/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/web-huda//_next/:path+",
+        "destination": "/web-huda/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
